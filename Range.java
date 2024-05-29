db5ea5c93ee5aca8eb7ab5e6692eb07006e8cc50
Nadav <141240505+NadavMenirav@users.noreply.github.com>
2024-05-29 12:29:36 +0300

    (feat) Range class

---
+++ Range.java
@@ -0,0 +1,34 @@
+public class Range() {
+    private int low;
+    private int high;
+
+
+    public Range(int low, int high) {
+        this.low = low;
+        this.high = high;
+    }
+
+    public int getLow() {
+        return this.low;
+    }
+
+    public int getHigh() {
+        return this.high;
+    }
+
+    public void setLow(int low) {
+        this.low = low;
+    }
+
+    public void setHigh(int high) {
+        this.high = high;
+    }
+
+    public boolean isXIncluded(int x) {
+        if (x >= this.low && x <= this.high) {
+            return true;
+        }
+        return fasle;
+    }
+
+}
