3ee9beb7eb3646ff2e2317836ec2d36fb08f1d5d
Nadav <141240505+NadavMenirav@users.noreply.github.com>
2024-05-30 11:28:18 +0300

    Add files via upload

---
+++ E1WhatsappNovember22.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class E1WhatsappNovember22 
+{	
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int x;
+		double y;
+		for (x=1;x<28;x=x+1) {
+			y=Math.pow(x,3)+6*Math.pow(x, 2)-10;
+			System.out.println(y); }
+	}
+}
