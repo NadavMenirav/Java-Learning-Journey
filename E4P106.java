1b512f103d73208d6d9f2f1e8d7c3af7f860d57f
Nadav <141240505+NadavMenirav@users.noreply.github.com>
2024-06-05 16:18:46 +0300

    (feat) E4P106

---
+++ E4P106.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class E4P106 {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		double a,b,c;
+		System.out.println("Enter 2 numbers");
+		a=input.nextDouble();
+		b=input.nextDouble();
+		c=Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
+		System.out.println(c);
+
+	}
+
+}
