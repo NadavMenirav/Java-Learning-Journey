c03d33396d5c9079488cb92d303bfdd1dcd0972d
Nadav <141240505+NadavMenirav@users.noreply.github.com>
2024-07-01 22:41:17 +0300

    Add files via upload

--- Node.java
+++ Node.java
@@ -1,69 +1,37 @@
-public class Node <T> {
-
-    private T value;
-
-    private Node<T> next;
-
-   
-
-    public Node (T value){
-
-          this.value = value;
-
-          this.next = null;
-
-    }
-
-   
-
-    public Node (T value, Node<T> next){
-
-          this.value = value;
-
-          this.next = next;
-
-    }
-
-
-
-    public T getValue() {
-
-          return value;
-
-    }
-
-
-
-    public void setValue(T value) {
-
-          this.value = value;
-
-    }
-
-
-
-    public Node<T> getNext() {
-
-          return next;
-
-    }
-
-
-
-    public void setNext(Node<T> next) {
-
-          this.next = next;
-
-    }
-
-   
-
-    public boolean hasNext(){
-
-          return (this.next != null);
-
-    }
-
-   
+public class Node<T> {
+	//תכונות המחלקה
+	private T value;
+	private Node<T> next;
+	//פעולות בונות
+	public Node(T value) {
+		this.value=value;
+		this.next=null;
+	}
+	public Node(T value,Node<T> next) {
+		this.value=value;
+		this.next=next;
+	}
+	public Node(int x) {
+		// TODO Auto-generated constructor stub
+	}
+	//פעולות מאחזרות
+	public T getValue() {
+		return this.value;
+	}
+	public Node<T> getNext(){
+		return this.next;
+	}
+	//פעולות משנות ערך
+	public void setValue(T value) {
+		this.value=value;
+	}
+	public void setNext(Node<T> next) {
+		this.next=next;
+	}
+	//פעולה שבודקת אם יש עוד חוליה אחרי
+	public boolean hasNext() {
+		return (this.next!=null);
+	}
+	
 
 }
